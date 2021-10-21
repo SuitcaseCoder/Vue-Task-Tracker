01665c1a403189538f0bc635d860c23403168b05
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-10-21 11:24:53 -0500

    added a proxy

--- src/App.vue
+++ src/App.vue
@@ -44,14 +44,14 @@ export default {
       )
     }, 
     async fetchTasks(){
-      const res = await fetch(`http://localhost:5000/tasks`)
+      const res = await fetch(`api/tasks`)
       
       const data = await res.json()
 
       return data;
     },
         async fetchTask(id){
-      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const res = await fetch(`api/tasks/${id}`)
       
       const data = await res.json()
 
@@ -59,7 +59,7 @@ export default {
     }
   },
   async created(){
-    this.tasks = this.fetchTasks()
+    this.tasks = await this.fetchTasks()
   }
 }
 </script>
