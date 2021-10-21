145e22dd5f52aa500a62c4567e56199e85ea2c93
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-10-21 11:54:41 -0500

    DELETE  backend

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,28 @@ export default {
     toggleAddTask(){
       this.showAddTask = !this.showAddTask
     },
-    addTask(newTask){
-      this.tasks = [...this.tasks, newTask]
+    async addTask(newTask){
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type':'application/json'
+        },
+        body: JSON.stringify(newTask)
+    })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
     },
-    deleteTask(id){
+    async deleteTask(id){
       if(confirm('Are you sure?')){
-      this.tasks = this.tasks.filter((task) => task.id !== id)
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'DELETE'
+      })
+      res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task');
+
+      
       }
     }, 
     toggleReminder(id){
